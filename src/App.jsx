c81f7052bc17252a7a910860efb6d8fa58ee9c29
Robbie Prevost <robbieprevost@gmail.com>
2017-07-01 06:26:18 -0700

    Doing some CRUD-class with IssueList Placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 const contentNode = document.getElementById('contents');
-const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}!`).join('');
-const component = <p>{message}</p>; // A simple JSX component
-ReactDOM.render(component, contentNode);  // render the component inside the content Node
+class IssueList extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue list.</div>
+        );
+    }
+}
+
+ReactDOM.render(<IssueList/>, contentNode); // render the component in the content Node
