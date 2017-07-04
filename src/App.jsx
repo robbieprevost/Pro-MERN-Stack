84bd7aa14bfd18c81b0c793464279de909981114
Robbie Prevost <robbieprevost@gmail.com>
2017-07-04 03:21:36 -0700

    Setting state- Creating a new Issue by timer.

--- src/App.jsx
+++ src/App.jsx
@@ -79,13 +79,38 @@ class IssueAdd extends React.Component {
 }
 
 class IssueList extends React.Component {
+    constructor() {
+        super();
+        this.state = { issues: issues};
+
+        setTimeout(this.createTestIssue.bind(this),2000);
+    }
+
+    createIssue(newIssue){
+        const newIssues = this.state.issues.slice();
+        newIssue.id = this.state.issues.length + 1;
+        newIssues.push(newIssue);
+        this.setState({ issues: newIssues});
+    }
+
+    createTestIssue() {
+        this.createIssue(
+            {
+                status: 'New',
+                owner: 'Pieta',
+                created: new Date(),
+                title: 'Completion date should be optional'
+            }
+        );
+    }
+
     render() {
-        return (
+        return(
             <div>
                 <h1>Issue Tracker</h1>
                 <IssueFilter />
                 <hr />
-                <IssueTable issues={issues}/>
+                <IssueTable issues={this.state.issues}/>
                 <hr />
                 <IssueAdd />
             </div>
