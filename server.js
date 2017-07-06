2d3890b23d06b328a2c49f1d737951fe0e912edc
Robbie Prevost <robbieprevost@gmail.com>
2017-07-06 06:36:48 -0700

    Implementing Express REST Apis - added some error handling

--- server.js
+++ server.js
@@ -26,6 +26,41 @@ const issues = [
     }
 ];
 
+const validIssueStatus = {
+    New: true,
+    Open: true,
+    Assigned: true,
+    Fixed: true,
+    Verified: true,
+    Closed: true
+};
+
+const issueFieldType = {
+    id: 'required',
+    status: 'required',
+    owner: 'required',
+    effort: 'optional',
+    created: 'required',
+    completionDate: 'optional',
+    title: 'required'
+};
+
+function validateIssue(issue){
+    for(const field in issueFieldType){
+        const type = issueFieldType[field];
+        if(!type) {
+            delete issue[field];
+        }else if(type === 'required' && !issue[field]){
+            return ` ${field} is required.`;
+        }
+    }
+
+    if(!validIssueStatus[issue.status])
+        return ` ${issue.status} is not a valid status.`;
+
+    return null;
+}
+
 app.get('/api/issues',(req,res)=> {
     const metadata = { total_count: issues.length};
     res.json({ _metadata:metadata, records:issues});
@@ -33,14 +68,19 @@ app.get('/api/issues',(req,res)=> {
 
 app.post('/api/issues', (req,res) => {
     const newIssue = req.body;
-    console.log(newIssue);
+    //console.log(newIssue);
     newIssue.id = issues.length + 1;
     newIssue.created = new Date();
     if(!newIssue.status)
         newIssue.status = 'New';
+    const err = validateIssue(newIssue);
+    if(err){
+        res.status(422).json({message: `Invalid request: ${err}`});
+        return;
+    }
 
     issues.push(newIssue);
-    console.log(newIssue);
+    //console.log(newIssue);
     res.json(newIssue);
 });
 
