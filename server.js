835ff0499aac463b9392722981c9c19048346830
Robbie Prevost <robbieprevost@gmail.com>
2017-07-06 12:24:47 -0700

    Added mongodb implementation, test, db population script, and frontend adjustments.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
 app.use(express.static('static'));
 app.use(bodyParser.json());
+
+let db;
 
 const issues = [
     {
@@ -62,8 +65,17 @@ function validateIssue(issue){
 }
 
 app.get('/api/issues',(req,res)=> {
-    const metadata = { total_count: issues.length};
-    res.json({ _metadata:metadata, records:issues});
+    db.collection('issues').find().toArray()
+        .then(issues => {
+            const metadata = {total_count: issues.length};
+            res.json({
+                _metadata: metadata,
+                records: issues
+            })
+        }).catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error: ${error}`});
+    });
 });
 
 app.post('/api/issues', (req,res) => {
@@ -84,6 +96,12 @@ app.post('/api/issues', (req,res) => {
     res.json(newIssue);
 });
 
-app.listen(3000, () => {
-    console.log('App started on port 3000')
+MongoClient.connect('mongodb://localhost/issuetracker')
+    .then(connection => {
+        db = connection;
+        app.listen(3000, () => {
+            console.log('App started on port 3000');
+        });
+    }).catch(error => {
+    console.log('ERROR', error);
 });
