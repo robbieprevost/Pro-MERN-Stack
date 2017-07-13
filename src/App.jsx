5d0416c95266b63235149773d6c6dadf240ac312
Robbie Prevost <robbieprevost@gmail.com>
2017-07-13 12:11:33 -0700

    Webpack integration - Dev Server

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import IssueList from './IssueList.jsx';
 
 const contentNode = document.getElementById('contents');
-// test
+
 
 ReactDOM.render(<IssueList />, contentNode);    // Render the component inside the content Node
--- webpack.config.js
+++ webpack.config.js
@@ -29,4 +29,13 @@ module.exports = {
             },
         ]
     },
+    devServer:{
+        port: 8000,
+        contentBase: 'static',
+        proxy: {
+            '/api/*':{
+                target: 'http://localhost:3000'
+            }
+        }
+    }
 };
